02f75358c8fc6e33d61cd7a8a074d3766a963ce2
Anwesh43 <anweshthecool0@gmail.com>
2018-04-16 08:01:01 +0530

    adding Animator class

--- dotsrotatorview/src/main/java/ui/anwesome/com/dotsrotatorview/DotsRotatorView.kt
+++ dotsrotatorview/src/main/java/ui/anwesome/com/dotsrotatorview/DotsRotatorView.kt
@@ -46,4 +46,33 @@ class DotsRotatorView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class Animator (var view : DotsRotatorView, var animated : Boolean = false) {
+
+        fun animate (updatecb : () -> Unit) {
+            if (animated) {
+                try {
+                    updatecb()
+                    Thread.sleep(50)
+                    view.invalidate()
+                }
+                catch(ex : Exception) {
+
+                }
+            }
+        }
+
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
+            }
+        }
+    }
 }
