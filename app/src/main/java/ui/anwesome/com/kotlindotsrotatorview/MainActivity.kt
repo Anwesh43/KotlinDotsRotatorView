5dd3b4e69693f16080ecef628c42a824cf35ebf2
Anwesh43 <anweshthecool0@gmail.com>
2018-04-16 08:15:06 +0530

    adding DotsRotatorView in activity

--- app/src/main/java/ui/anwesome/com/kotlindotsrotatorview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlindotsrotatorview/MainActivity.kt
@@ -2,11 +2,12 @@ package ui.anwesome.com.kotlindotsrotatorview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.dotsrotatorview.DotsRotatorView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        DotsRotatorView.create(this)
     }
 }
